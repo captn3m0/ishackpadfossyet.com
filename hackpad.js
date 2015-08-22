8646642aa436dcab54905cf17f78f3fc8103e236
Abhay Rana <capt.n3m0@gmail.com>
2015-08-22 14:41:23 +0530

    Dropbox released it!

--- hackpad.js
+++
@@ -1,7 +0,0 @@
-var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-var firstDate = new Date();
-var secondDate = new Date("2015-04-10");
-
-var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
-
-document.getElementById('days').innerText = diffDays;
